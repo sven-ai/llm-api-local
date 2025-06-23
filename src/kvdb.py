518f9bf3582c2688f49cf0918bba3044e1d12c10
Anton <anton@antonsjeans.com>
2025-06-23 19:04:29 +0200

    yo

--- src/kvdb.py
+++ src/kvdb.py
@@ -1,14 +1,12 @@
-import sqlite3
-
 from utils import *
 
-_connection = sqlite3.connect(
-    "/data/kvs.db",
-    check_same_thread=False,
-    autocommit=True,
-    # autocommit=False,
-)
-_connection.execute("PRAGMA journal_mode=WAL")
+# _connection = sqlite3.connect(
+#     "/data/kvs.db",
+#     check_same_thread=False,
+#     autocommit=True,
+#     # autocommit=False,
+# )
+# _connection.execute("PRAGMA journal_mode=WAL")
 
 
 _collections = LimitedDict(max_size=100)
@@ -18,59 +16,64 @@ class KVCollection:
     def __init__(self, collection: str):
         self.collection = collection
 
-        with _connection:
-            cursor = _connection.cursor()
+        print("yo")
+        # with _connection:
+        #     cursor = _connection.cursor()
 
-            cursor.execute(
-                f"CREATE TABLE IF NOT EXISTS {collection} (key TEXT NOT NULL, val TEXT NOT NULL)"
-            )
-            cursor.execute(
-                f"CREATE UNIQUE INDEX IF NOT EXISTS idx_key ON {collection} (key)"
-            )
-            _connection.commit()
+        #     cursor.execute(
+        #         f"CREATE TABLE IF NOT EXISTS {collection} (key TEXT NOT NULL, val TEXT NOT NULL)"
+        #     )
+        #     cursor.execute(
+        #         f"CREATE UNIQUE INDEX IF NOT EXISTS idx_key ON {collection} (key)"
+        #     )
+        #     _connection.commit()
 
     def get(self, key: str) -> str | None:
-        with _connection:
-            cursor = _connection.cursor()
+        print("yo")
+        return None
+        # with _connection:
+        #     cursor = _connection.cursor()
 
-            rows = cursor.execute(
-                f"SELECT val FROM {self.collection} WHERE key = ?", [key]
-            ).fetchone()
+        #     rows = cursor.execute(
+        #         f"SELECT val FROM {self.collection} WHERE key = ?", [key]
+        #     ).fetchone()
 
-            if rows:
-                # print(
-                #     f"KVDB.get {self.collection} : ({key}) -> {len(rows[0])}-bytes"
-                # )
-                return rows[0]
+        #     if rows:
+        #         # print(
+        #         #     f"KVDB.get {self.collection} : ({key}) -> {len(rows[0])}-bytes"
+        #         # )
+        #         return rows[0]
 
-            # print(f"KVDB.get({key}) -> None")
-            return None
+        #     # print(f"KVDB.get({key}) -> None")
+        #     return None
 
     def set(self, key: str, val: str):
-        # print(f"KVDB.set: {self.collection} : [ {key} : {len(val)}-bytes ]")
+        print("yo")
+        # # print(f"KVDB.set: {self.collection} : [ {key} : {len(val)}-bytes ]")
 
-        with _connection:
-            cursor = _connection.cursor()
+        # with _connection:
+        #     cursor = _connection.cursor()
 
-            cursor.execute(
-                f"INSERT OR IGNORE INTO {self.collection} VALUES (?, ?)",
-                [key, val],
-            )
-            cursor.execute(
-                f"UPDATE {self.collection} SET val = ? WHERE key = ?",
-                [val, key],
-            )
+        #     cursor.execute(
+        #         f"INSERT OR IGNORE INTO {self.collection} VALUES (?, ?)",
+        #         [key, val],
+        #     )
+        #     cursor.execute(
+        #         f"UPDATE {self.collection} SET val = ? WHERE key = ?",
+        #         [val, key],
+        #     )
 
-            _connection.commit()
+        #     _connection.commit()
 
     def delete(self, key: str):
-        with _connection:
-            cursor = _connection.cursor()
+        print("yo")
+        # with _connection:
+        #     cursor = _connection.cursor()
 
-            cursor.execute(
-                f"DELETE FROM {self.collection} WHERE key = ?", [key]
-            )
-            _connection.commit()
+        #     cursor.execute(
+        #         f"DELETE FROM {self.collection} WHERE key = ?", [key]
+        #     )
+        #     _connection.commit()
 
 
 def kvdb_get_collection(name: str) -> KVCollection:
